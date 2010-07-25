0aa7d4a294c1ec7ff162285616ed2e20f7b1ff92
Igor Serebryany <igor47@moomers.org>
2010-07-25 09:30:51 +0000

    added framework for testing the highlighting

---
+++ highlighttests.py
@@ -0,0 +1,18 @@
+#Unit tests file for highlights
+#http://www.yelp.com/search?find_desc=deep+dish+pizza&ns=1&rpp=10&find_loc=San+Francisco%2C+CA
+
+import unittest
+
+class TestHighlights(unittest.TestCase):
+	"""Tests the highlights class for to make sure it works"""
+	def setUp(self):
+		pass
+
+	def testSingle(self):
+		"""Make sure that snippets get highlighted"""
+		pass
+
+	def testConsecutive(self):
+		"""Consecutive highlighted words should be grouped in one highlight"""
+		pass
+
