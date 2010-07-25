e25ca71db92e273055c6efd3055cab95c0d190c6
Igor Serebryany <igor47@moomers.org>
2010-07-25 22:07:05 +0000

    created files for snippet test

---
+++ snippets.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#
+# Igor Serebryany
+# Yelp Code Test 7/25/09
+
+"""Does the yelp puzzle of snippet highlighting"""
+
+def highlight_doc(doc, query):
+	"""Highlights snippets in a document
+	Args:
+		doc -- document to be highlighted
+		query -- the search string
+
+	Returns:
+		The most relevant snippets from the document with the search terms highlighted."""
+
+	return ""
+
